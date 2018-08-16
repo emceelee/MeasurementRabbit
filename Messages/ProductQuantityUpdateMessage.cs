832e23ec9b7816fb8a6392973a569aad757c84ed
Matthew Lee <matthew_lee@qbsol.com>
2018-08-16 15:17:42 -0500

    ProductQuantityUpdateMessage ToString

--- Messages/ProductQuantityUpdateMessage.cs
+++ Messages/ProductQuantityUpdateMessage.cs
@@ -10,5 +10,12 @@ namespace Messages
 
         public DateTime ContractDayStart { get; set; }
         public DateTime ContractDayEnd { get; set; }
+
+        public override string ToString()
+        {
+            return $@"MeasurementPointId: {MeasurementPointId}, 
+        ContractDayStart: {ContractDayStart.ToString("MM/dd/yyyy")}, 
+        ContractDayEnd: {ContractDayEnd.ToString("MM/dd/yyyy")}";
+        }
     }
 }
